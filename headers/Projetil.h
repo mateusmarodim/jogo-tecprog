1800c76e2b6dcbe4fc97efe4513a9dbad02d228c
Mateus Marodim <v.marodim@gmail.com>
2021-05-03 10:18:57 -0300

    Update Projetil.h

--- headers/Projetil.h
+++ headers/Projetil.h
@@ -1,16 +1,11 @@
 #pragma once
 #include "EntidadeColidivel.h"
 
-class Projetil: public EntidadeColidivel
+class Espinho: public EntidadeColidivel
 {
-private:
-   // bool deletar;
 public:
-    Projetil(Vetor2F Pos = (0.0,0.0), Vetor2F Vel = (0.0,0.0), Vetor2F Tam = (0.0,0.0));
-    ~Projetil();
+    Espinho(Vetor2F Pos = (0.0,0.0), Vetor2F Vel = (0.0,0.0), Vetor2F Tam = (0.0,0.0));
+    ~Espinho();
     virtual void atualizar(float f);
     virtual void colidir(EntidadeColidivel* outro, string tipoEntidade);
-    
-   // void setDeletar();
-   // bool getDeletar();
 };
